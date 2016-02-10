ac1d4e99c9a642faa54573397981613f92cd8c09
Philippe Dionne <dionne.phil@gmail.com>
2016-02-10 11:26:03 -0500

    Add mongoid generator

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module CitizenBudget
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
 
+    config.generators do |g|
+      g.orm :mongoid
+    end
   end
 end
