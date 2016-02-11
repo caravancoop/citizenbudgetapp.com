4571b77c89ff297dcb27df445965975c4fd629fa
Philippe Dionne <dionne.phil@gmail.com>
2016-02-11 16:59:10 -0500

    Create google_api_authorizations table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211213700) do
+ActiveRecord::Schema.define(version: 20160211215427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20160211213700) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+  end
+
+  create_table "google_api_authorizations", force: :cascade do |t|
+    t.integer  "questionnaire_id"
+    t.jsonb    "token",            default: {}
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
   end
 
   create_table "organizations", force: :cascade do |t|
