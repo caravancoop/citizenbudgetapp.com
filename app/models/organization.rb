1acaa975a5fafa25408a3d3e41881697b4190b56
Philippe Dionne <dionne.phil@gmail.com>
2016-02-11 12:20:43 -0500

    Create organizations table

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,11 +1,6 @@
-class Organization
-  include Mongoid::Document
-  include Mongoid::Paranoia
-
+class Organization < ActiveRecord::Base
   has_many :admin_users
   has_many :questionnaires
-
-  field :name, type: String
 
   validates :name, presence: true
 end
