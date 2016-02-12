0319dc8053ec0cc11068c59a988aa4bab6f1c642
Philippe Dionne <dionne.phil@gmail.com>
2016-02-12 11:10:36 -0500

    Use bower managed jquery and jquery-ujs

--- Gemfile
+++ Gemfile
@@ -46,9 +46,6 @@ gem 'faraday'
 # Rake
 gem 'ruby-progressbar'
 
-# Assets
-gem 'jquery-rails'
-
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery_ujs
+//= require jquery/dist/jquery
+//= require jquery-ujs/src/rails
 //= require_tree ./libs
 //= require_tree ./plugins
 //= require i18n
