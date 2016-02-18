273f935ebc628d9bc8594830f77e6d13ccfd0d19
Philippe Dionne <dionne.phil@gmail.com>
2016-02-18 09:39:27 -0500

    Create a sample

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-u = AdminUser.create(email: 'test@example.com', password: 'testexample', role: 'superuser')
-o = Organization.create(name: 'Test')
-q = Questionnaire.create(organization: o, title: 'Questionnaire title', mode: 'services')
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
