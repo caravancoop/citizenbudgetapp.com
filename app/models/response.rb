d6f153cf92f389ea46f78f457d2e6ac06b8074c9
Philippe Dionne <dionne.phil@gmail.com>
2016-02-18 13:40:31 -0500

    Refactor response with presenter

--- app/models/response.rb
+++ app/models/response.rb
@@ -54,16 +54,6 @@ class Response < ActiveRecord::Base
     balance
   end
 
-  # @return [String] the full first name and last name initial
-  def display_name
-    if name?
-      parts = name.strip.split(' ', 2)
-      parts[0] = UnicodeUtils.titlecase(parts[0]) if parts[0][/\A\p{Ll}/]
-      parts[1] = "#{UnicodeUtils.upcase(parts[1][0])}." if parts[1]
-      parts.join ' '
-    end
-  end
-
   # Performs validations outside create or update operations.
   #
   # @return [Hash] any validation errors
