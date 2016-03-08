e055eb277558ff3e511bfb8a4232a186cedbcfcc
Philippe Dionne <dionne.phil@gmail.com>
2016-03-08 15:34:50 -0500

    Add criterion to sections

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308170226) do
+ActiveRecord::Schema.define(version: 20160308203302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -173,8 +173,9 @@ ActiveRecord::Schema.define(version: 20160308170226) do
     t.string   "embed"
     t.string   "group"
     t.integer  "position"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.text     "criterion",        default: [],              array: true
   end
 
   add_index "sections", ["position"], name: "index_sections_on_position", using: :btree
