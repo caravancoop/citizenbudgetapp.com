24d530c5048463ebcead7d383c3bfa6b2afa77e0
Philippe Dionne <dionne.phil@gmail.com>
2016-03-16 17:11:38 -0400

    Configure carrierwave only if env vars are present

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,14 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region                 => ENV['AWS_REGION']
-  }
-  config.fog_directory  = ENV['AWS_DIRECTORY']
-  # config.asset_host     = 'https://assets.example.com'
-  config.fog_public     = true
-  config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
+if ENV['AWS_ACCESS_KEY_ID']
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region                 => ENV['AWS_REGION']
+    }
+    config.fog_directory  = ENV['AWS_DIRECTORY']
+    # config.asset_host     = 'https://assets.example.com'
+    config.fog_public     = true
+    config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
+  end
 end
